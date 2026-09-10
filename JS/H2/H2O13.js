857ea88fab8a5a528187db4feab7918204fad8e0
nielsjanf <24507@leerling.pieterzandt.nl>
2026-09-10 13:08:43 +0000

    13

--- JS/H2/H2O13.js
+++ JS/H2/H2O13.js
@@ -12,7 +12,11 @@ var yJos = 300;
 
 function preload() {
   brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
-  frame = loadImage("images/sprites/Jos100px/Jos_0.png");
+
+  for (var b = 0;b < aantalBeeldjes;b++){
+    frame = loadImage("images/sprites/Jos100px/Jos_"+ b +".png");
+    animatie.push(frame)
+  }
 }
 
 function setup() {
@@ -25,24 +29,29 @@ function setup() {
 function draw() {
   background(brug);
   tekenRaster();
-
+  
   if (keyIsDown(LEFT_ARROW)) {
     xJos -= celGrootte;
+    nummer = 2
   }
   if (keyIsDown(RIGHT_ARROW)) {
     xJos += celGrootte;
+    nummer = 1
   }
   if (keyIsDown(UP_ARROW)) {
     yJos-=celGrootte;
+    nummer = 4
   }
   if (keyIsDown(DOWN_ARROW)) {
     yJos += celGrootte;
+    nummer = 5
   }
   
   xJos = constrain(xJos,0,width - celGrootte);
   yJos = constrain(yJos,0,height - celGrootte);
   
-  image(frame,xJos,yJos);
+  
+  image(animatie[nummer],xJos,yJos);
 }
 
 function tekenRaster() {
